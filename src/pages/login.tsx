13ce2daa689ecd8cb9c208b817691f8e374306e9
Jomark Pangan <jomarkrazonpangan@gmail.com>
2023-04-29 14:22:44 +0800

    Created a simple Login Layout

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,10 +1,10 @@
-import { signIn } from 'next-auth/react';
 import Head from 'next/head';
 import { FaDiscord, FaGoogle } from 'react-icons/fa';
-import { BiTestTube } from 'react-icons/bi';
+import { RiTestTubeFill } from 'react-icons/ri';
 import { GetServerSidePropsContext } from 'next';
 import { getServerAuthSession } from '@/server/auth';
-import { toast } from 'react-toastify';
+import { Button } from '@/components/Elements';
+import { AiFillGold } from 'react-icons/ai';
 
 export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
   const session = await getServerAuthSession(ctx);
@@ -30,40 +30,66 @@ export default function Login() {
         <title>Login | Money Manager</title>
       </Head>
 
-      <div className="flex h-screen items-center justify-center">
-        <div className="flex flex-col gap-2 rounded-lg bg-white p-4 shadow">
-          <button
-            onClick={() =>
-              signIn('discord', {
-                callbackUrl: '/',
-              })
-            }
-            className="flex items-center gap-2 rounded-lg bg-primary px-4 py-2 font-semibold text-primary-contrast shadow-primary-light transition-all hover:bg-primary-dark"
-          >
-            <FaDiscord className="text-2xl" />
-            Login via Discord
-          </button>
+      <div className="mx-auto flex h-screen items-center justify-center p-4">
+        <div className="mx-auto flex w-full max-w-xs flex-col items-center gap-4 rounded-xl border border-gray-200 p-4">
+          <div className="flex flex-col items-center">
+            <AiFillGold className="text-4xl" />
+            <p className="text-xl">Momney Manager</p>
+          </div>
 
-          <button
-            onClick={() =>
-              signIn('google', {
-                callbackUrl: '/',
-              })
-            }
-            className="flex items-center gap-2 rounded-lg bg-primary px-4 py-2 font-semibold text-primary-contrast shadow-primary-light transition-all hover:bg-primary-dark"
-          >
-            <FaGoogle className="text-2xl" />
-            Login via Google
-          </button>
-          <button
-            onClick={() => toast.info('Coming Soon!')}
-            className="flex items-center gap-2 rounded-lg bg-primary px-4 py-2 font-semibold text-primary-contrast shadow-primary-light transition-all hover:bg-primary-dark"
-          >
-            <BiTestTube className="text-2xl" />
-            Try Demo Version
-          </button>
+          <div className="flex flex-col gap-2">
+            <Button primary className="flex items-center">
+              <FaDiscord className="mr-2 text-2xl" />
+              Login via Discord
+            </Button>
+
+            <Button primary className="flex items-center">
+              <FaGoogle className="mr-2 text-2xl" />
+              Login via Google
+            </Button>
+
+            <Button className="flex items-center">
+              <RiTestTubeFill className="mr-2 text-2xl" />
+              Try Demo
+            </Button>
+          </div>
         </div>
       </div>
     </>
   );
+}
+
+{
+  /* <div className="w-full rounded p-4">
+  <p className="mb-4 text-center text-4xl font-semibold">momney</p>
+
+  <div className="flex flex-col items-center gap-2 p-4">
+    <Button
+      className="flex min-w-[160px] items-center py-2"
+      primary
+      onClick={() => signIn('discord', { callbackUrl: '/' })}
+    >
+      <FaDiscord className="mr-2 text-2xl" />
+      Login via Discord
+    </Button>
+
+    <Button
+      className="flex min-w-[160px] items-center py-2"
+      primary
+      onClick={() => signIn('discord', { callbackUrl: '/' })}
+    >
+      <FaGoogle className="mr-2 text-2xl" />
+      Login via Google
+    </Button>
+
+    <Button
+      className="flex min-w-[160px] items-center py-2"
+      primary
+      onClick={() => signIn('discord', { callbackUrl: '/' })}
+    >
+      <BiTestTube className="mr-2 text-2xl" />
+      Try Demo
+    </Button>
+  </div>
+</div>; */
 }
